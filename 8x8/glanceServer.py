281c2c70dfa050268e331f8161e50889f9b384b4
Mudit Gupta <muditgupta.jaipur@gmail.com>
2015-03-22 18:54:10 -0400

    added icon parser to main server

--- 8x8/glanceServer.py
+++ 8x8/glanceServer.py
@@ -5,11 +5,30 @@ import datetime
 from Adafruit_8x8 import EightByEight
 from flask import Flask
 from flask.ext.restful import Api, Resource
+import os
+from PIL import Image
 
 
 app = Flask(__name__)
 api = Api(app)
 grid = []
+icons = {}
+
+for d, r, files in os.walk("icons/"):
+	for filename in files:
+		#print filename
+		im = Image.open("icons/"+filename, 'r')
+		pixels = im.load() # this is not a list, nor is it list()'able
+		width, height = im.size
+		all_pixels = []
+		for x in range(width):      
+			for y in range(height):
+				cpixel = pixels[x, y]
+				all_pixels.append(cpixel)
+		#print all_pixels
+        icons[filename]=all_pixels
+
+print icons
 for i in range(0,8):
   grid.append(EightByEight(address=0x70+i))
 
