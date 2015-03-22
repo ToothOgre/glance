ecc3de955fdbea6dcbdd3a42b2e4c5a835636a15
Mudit Gupta <muditgupta.jaipur@gmail.com>
2015-03-21 23:07:06 -0400

    fixed access

--- 8x8/glanceServer.py
+++ 8x8/glanceServer.py
@@ -19,6 +19,7 @@ class Matrix8X8(Resource):
     pass
 
   def put(self, id):
+    print"PUT"
     for x in range(0, 8):
       for y in range(0, 8):
         grid[id].setPixel(x, y)
@@ -37,6 +38,6 @@ api.add_resource(Matrix8X8, '/8x8/<int:id>', endpoint = '8x8')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
 
 
