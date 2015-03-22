db65dafc8641b3152c5927bb15a5e7528c68910e
Mudit Gupta <muditgupta.jaipur@gmail.com>
2015-03-22 18:19:24 -0400

    fixed

--- 8x8/test_image_parse.py
+++ 8x8/test_image_parse.py
@@ -1,6 +1,15 @@
 import os
 from PIL import Image
 
-for d, r, f in os.walk("icons\"):
-	for filename in f:
+for d, r, files in os.walk("icons\"):
+	for filename in files:
 		print filename
+		im = Image.open(filename, 'r')
+		pixels = i.load() # this is not a list, nor is it list()'able
+		width, height = i.size
+		all_pixels = []
+		for x in range(width):
+			for y in range(height):
+				cpixel = pixels[x, y]
+				all_pixels.append(cpixel)
+		print all_pixels
