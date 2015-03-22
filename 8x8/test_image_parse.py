686114de0bdf949ed217db0dfe3812d8f648fefa
Mudit Gupta <muditgupta.jaipur@gmail.com>
2015-03-22 18:09:26 -0400

    Revert "added image pixel parsing"
    This reverts commit 2c6c872e2cec5c1bfa3bb7bd9623fd18b4ad2cf7.

--- 8x8/test_image_parse.py
+++ 8x8/test_image_parse.py
@@ -1,15 +1,6 @@
 import os
 from PIL import Image
 
-for d, r, files in os.walk("icons\"):
-	for filename in files:
+for d, r, f in os.walk("icons\"):
+	for filename in f:
 		print filename
-		im = Image.open(filename, 'r')
-		pixels = i.load() # this is not a list, nor is it list()'able
-		width, height = i.size
-		all_pixels = []
-		for x in range(width):
-			for y in range(height):
-				cpixel = pixels[x, y]
-				all_pixels.append(cpixel)
-		print all_pixels
